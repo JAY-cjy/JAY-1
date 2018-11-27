c56ed458c9675393d716b929783632fe88a43c78
Jay <985533194@.com>
2018-11-27 20:51:01 +0800

    '连接数据库'

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ignoreRouter = require('./config/ignoreRouter');
+//引入自定义模块
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,16 +21,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//调用自定义的中间件函数，用来判断用户是否登录
+app.use(function (req, res, next) {
+  //排除登录和注册页面
+  if (ignoreRouter.indexOf(req.url) > -1) {
+    next();
+    return;
+  }
+
+  var nickname = req.cookies.nickname;
+
+
+  next();
+
+})
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+
 var router = express.Router();
+var url = 'mongodb://127.0.0.1:27017';
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+    if (err) {
+      //连接数据库失败
+      console.log('连接数据库是失败', err);
+      res.render('error', {
+        message: '连接数据库失败',
+        error: err
+      });
+      return;
+    }
+
+    var db = client.db('JAY-project');//数据库名字
+
+    //user集合名字
+    db.collection('user').find().toArray(function (err, data) {
+      if (err) {
+        console.log('查询用户数据失败', err);
+        //有错误就渲染errot.ejs文件
+        res.render('error', {
+          message: '查询失败',
+          error: err
+        });
+      } else {
+        console.log(data);
+        res.render('users', {
+          list: data
+        });
+      }
+      client.close(); //关闭数据库的链接
+    });
+
+  });
+
+  // res.render('users'); 
+  //这里不能写了，因为mongodb的操作时异步操作，
+  //写这个可能会出现这先运行的可能，然后就运行下面得了
 });
 
 module.exports = router;
